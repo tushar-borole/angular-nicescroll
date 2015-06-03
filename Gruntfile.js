51ba079c49be1497d168d155e0c97de053337c83
Tushar Borole <tushariscoolster@gmail.com>
2015-06-03 23:25:32 +0530

    Initial commit

---
+++ Gruntfile.js
@@ -0,0 +1,57 @@
+'use strict';
+
+/* jshint camelcase: false */
+
+
+module.exports = function (grunt)
+{
+    // Load grunt tasks automatically
+    require('load-grunt-tasks')(grunt);
+
+    // Time how long tasks take. Can help when optimizing build times
+    require('time-grunt')(grunt);
+
+    var appConfig = {
+        app: 'src',
+        example: 'example',
+        dist: 'dist',
+        tpl: 'src/templates'
+    };
+    // Define the configuration for all the tasks
+    grunt.initConfig({
+        // Watches files for changes and runs tasks based on the changed files
+        appConfig: appConfig,
+        bump: {
+            options: {
+                files: ['bower.json','package.json'],
+                updateConfigs: [],
+                commit: true,
+                commitMessage: 'Release v%VERSION%',
+                commitFiles: [
+                    'bower.json',
+                    'angular-nicescroll.js',
+                    'package.json'
+                ],
+                createTag: true,
+                tagName: 'v%VERSION%',
+                tagMessage: 'Version %VERSION%',
+                push: false,
+                pushTo: 'git@github.com:tushariscoolster/angular-nicescroll.git',
+                gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d',
+                globalReplace: false
+            }
+        }
+    });
+
+
+
+
+
+    grunt.registerTask('release', function (target)
+    {
+        grunt.task.run([
+            'bump'
+        ]);
+    });
+
+};
---
+++ demo/app/view1/view1.js
@@ -0,0 +1,19 @@
+'use strict';
+
+angular.module('myApp.view1', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/view1', {
+    templateUrl: 'view1/view1.html',
+    controller: 'View1Ctrl'
+  });
+}])
+
+.controller('View1Ctrl', ['$scope',function($scope) {
+    
+    $scope.endScroll=function(){
+        alert("scrollend")
+    
+    }
+
+}]);
